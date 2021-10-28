950fe33c2c30d2970614cb9eccaa02576106c133
ChavBox <havenec@wwu.edu>
2021-10-28 14:53:02 -0700

    Adding homework 5

---
+++ Homework5/elizabot/elizadata.js
@@ -0,0 +1,130 @@
+// data for elizabot.js
+// entries prestructured as layed out in Weizenbaum's description
+// [cf: Communications of the ACM, Vol. 9, #1 (January 1966): p 36-45.]
+
+var elizaInitials = [
+ "Hello, welcome to Online Climbing Help, my name is Morgan and I'm here to answer any climbing question you have!",
+ "Hi, I'm Morgan. Feel free to ask me anything about climbing!"
+];
+
+var elizaFinals = [
+"See ya! Good luck on your next route.",
+// additions (not original)
+"I'm glad I could help, now go send!",
+"All out of questions? That's alright. I'll be here. Waiting. Forever...",
+"Enjoy the rest of your day! And remember, a not neat knot is a knot not needed.",
+"Maybe we could discuss this moreover in our next session ?   Goodbye."
+];
+
+var elizaQuits = [
+"stop",
+"bye",
+"goodbye",
+"done",
+"exit",
+"quit"
+];
+
+var elizaPres = [
+"dont", "don't",
+"cant", "can't",
+"wont", "won't",
+"recollect", "remember",
+"recall", "remember",
+"dreamt", "dreamed",
+"dreams", "dream",
+"maybe", "perhaps",
+"certainly", "yes",
+"machine", "computer",
+"machines", "computer",
+"computers", "computer",
+"were", "was",
+"you're", "you are",
+"i'm", "i am",
+"same", "alike",
+"identical", "alike",
+"equivalent", "alike"
+];
+
+var elizaPosts = [
+"am", "are",
+"your", "my",
+"me", "you",
+"myself", "yourself",
+"yourself", "myself",
+"i", "you",
+"you", "I",
+"my", "your",
+"i'm", "you are"
+];
+
+var elizaSynons = {
+"be": ["am", "is", "are", "was"],
+"belief": ["feel", "think", "believe", "wish"],
+"cannot": ["can't"],
+"desire": ["want", "need"],
+"everyone": ["everybody", "nobody", "noone"],
+"family": ["mother", "mom", "father", "dad", "sister", "brother", "wife", "children", "child"],
+"happy": ["elated", "glad", "better"],
+"sad": ["unhappy", "depressed", "sick"]
+};
+
+var elizaKeywords = [
+
+/*
+  Array of
+  ["<key>", <rank>, [
+    ["<decomp>", [
+      "<reasmb>",
+      "<reasmb>",
+      "<reasmb>"
+    ]],
+    ["<decomp>", [
+      "<reasmb>",
+      "<reasmb>",
+      "<reasmb>"
+    ]]
+  ]]
+*/
+
+["xnone", 0, [
+ ["*", [
+     "Wait... so do you climb or not?",
+     "Can you be more specific? I'm not sure how that's about climbing.",
+     "I'm happy to talk about climbing some more!"
+  ]]
+]],
+
+["name", 15, [
+ ["*", [
+     "Nice to meet you!",
+     "I'm glad that we've been introduced.",
+     "What a wonderful name that is."
+  ]]
+]],
+
+["potato", 100, [
+  ["* don't say *", [
+    "Okay"
+  ]],
+  ["say *", [
+    "Potato."
+  ]]
+]]
+
+];
+
+
+// regexp/replacement pairs to be performed as final cleanings
+// here: cleanings for multiple bots talking to each other
+var elizaPostTransforms = [
+	/ old old/g, " old",
+	/\bthey were( not)? me\b/g, "it was$1 me",
+	/\bthey are( not)? me\b/g, "it is$1 me",
+	/Are they( always)? me\b/, "it is$1 me",
+	/\bthat your( own)? (\w+)( now)? \?/, "that you have your$1 $2 ?",
+	/\bI to have (\w+)/, "I have $1",
+	/Earlier you said your( own)? (\w+)( now)?\./, "Earlier you talked about your $2."
+];
+
+// eof
