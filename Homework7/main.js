01e258602626cc97d5e92d30b40e1a1a7c85c8be
ChavBox <havenec@wwu.edu>
2021-11-09 18:11:50 -0800

    .

--- Homework7/main.js
+++ Homework7/main.js
@@ -4,10 +4,15 @@ $(document).ready(function() {
   var step2 = "This time, as you walk, focus on your breath. Before you start, take a minute to close your eyes and focus on your breathing. Once you've found your breath, follow it as you walk.";
   var step3 = "To complete this meditation, walk the stairs one last time. This time, try to connect your breathing and your body by taking each step with the natural rise and fall of your breath. Allow your foot come off the ground as you inhale, setting it down as you exhale.";
   var finished = "Congratulations on completing this mindfullness exercise! If it felt difficult, don't be discouraged, mindfullness is something that takes practice."
+
+  //user has not pressed the "next" button
   var count = 0;
+
+  //hide text to only show h1
   document.getElementById('text').style.display = "none";
 
     $('#next').click(function(){
+      
     if(count == 0){
       $('#intro').fadeOut(500);
       setTimeout(function(){
