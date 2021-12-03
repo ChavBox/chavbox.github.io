703826d41bf0dc38efd9464210de8bc55c0f391e
ChavBox <havenec@wwu.edu>
2021-12-02 21:29:05 -0800

    More work

---
+++ Final_Project_WebDev/js/main.js
@@ -0,0 +1,98 @@
+window.onload = function(){
+	var fact0 = "Fungi are genetically closer to humans than plants.";
+	var fact1 = "Fungi breath oxygen and exhale CO<sub>2</sub>.";
+	var fact2 = "One cubic inch of soil can contain enough mycelium to stretch over eight miles.";
+	var fact3 = "Mushrooms are the 'fruit' of myceluim, and is only 5 percent of the organism.";
+	var fact4 = "The largest organism on earth is a honey fungus, capable of spreading 2.4 miles across.";
+	var fact5 = "Mushroom spores are made of the hardest plant material, sporopollenin.";
+	var fact6 = "Mushroom spores can survive the vaccum and radiation of space.";
+	var fact7 = "Mushrooms will produce Vitamin D when exposed to UV light, although they don't require light to grow.";
+	var fact8 = "The first antibiotic ever used in medicine is derived from a fungi: Penicillin Mold.";
+	var fact9 = "There are several species of fungi that are bioluminescent, glowing as they grow.";
+
+	switch (Math.floor(Math.random()*10)) {
+		case 0:
+			document.getElementById('fact').innerHTML = fact0;
+			break;
+
+		case 1:
+			document.getElementById('fact').innerHTML = fact1;
+			break;
+
+		case 2:
+			document.getElementById('fact').innerHTML = fact2;
+			break;
+
+		case 3:
+			document.getElementById('fact').innerHTML = fact3;
+			break;
+
+		case 4:
+			document.getElementById('fact').innerHTML = fact4;
+			break;
+
+		case 5:
+			document.getElementById('fact').innerHTML = fact5;
+			break;
+
+		case 6:
+			document.getElementById('fact').innerHTML = fact6;
+			break;
+
+		case 7:
+			document.getElementById('fact').innerHTML = fact7;
+			break;
+
+		case 8:
+			document.getElementById('fact').innerHTML = fact8;
+			break;
+
+		case 9:
+			document.getElementById('fact').innerHTML = fact9;
+			break;
+	}
+
+	document.getElementById('newFact').onclick = function(){
+		switch (Math.floor(Math.random()*10)) {
+			case 0:
+				document.getElementById('fact').innerHTML = fact0;
+				break;
+
+			case 1:
+				document.getElementById('fact').innerHTML = fact1;
+				break;
+
+			case 2:
+				document.getElementById('fact').innerHTML = fact2;
+				break;
+
+			case 3:
+				document.getElementById('fact').innerHTML = fact3;
+				break;
+
+			case 4:
+				document.getElementById('fact').innerHTML = fact4;
+				break;
+
+			case 5:
+				document.getElementById('fact').innerHTML = fact5;
+				break;
+
+			case 6:
+				document.getElementById('fact').innerHTML = fact6;
+				break;
+
+			case 7:
+				document.getElementById('fact').innerHTML = fact7;
+				break;
+
+			case 8:
+				document.getElementById('fact').innerHTML = fact8;
+				break;
+
+			case 9:
+				document.getElementById('fact').innerHTML = fact9;
+				break;
+		}
+	}
+}
